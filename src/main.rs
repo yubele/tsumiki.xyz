da3bdb4847ca7b32b276fcbdcf725850551fc8cc
yusuke <yusuke@newsdict.jp>
2017-04-22 03:41:03 +0000

    initialize nickel.rs

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#[macro_use] extern crate nickel;
+use std::collections::HashMap;
+use nickel::{Nickel, HttpRouter};
+
+fn main() {
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            let mut data = HashMap::new();
+            data.insert("copyright_year", "2017");
+            return _res.render("templates/welcome/comingsoon.mustache", &data);
+        }
+    });
+
+    server.listen("127.0.0.1:6767");
+}
